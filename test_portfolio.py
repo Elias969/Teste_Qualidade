750ced9c394bc83835f99b97f9f0592d02c84ac6
Elias969 <93712140+Elias969@users.noreply.github.com>
2025-05-09 11:52:20 -0300

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -3,23 +3,20 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-# Caminho para o ChromeDriver (ajuste se necessário)
-# Você pode baixar em: https://chromedriver.chromium.org/downloads
 service = Service()
 
-# Inicializa o navegador
 driver = webdriver.Chrome(service=service)
 
 try:
-    # Abre o site hospedado na Vercel
+   
     driver.get("https://meu-portfolio-qrxh.vercel.app/")
     time.sleep(3)  # Aguarda 3 segundos para a página carregar
 
-    # Testa se o título da página contém "React App"
+  
     assert "React App" in driver.title
     print("✅ Página carregada com sucesso e título validado!")
 
-    # Testa se existe um elemento com a tag H1 (se necessário, altere para outro elemento)
+  
     header = driver.find_element(By.TAG_NAME, "h1")
     print("✅ H1 encontrado com texto:", header.text)
 
